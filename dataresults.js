8f21a4f224fff02c44d30c134771aeed63603a0b
drjpoudel <drjpoudel@gmail.com>
2024-08-01 09:04:05 +0545

    Update dataresults.js

--- dataresults.js
+++ dataresults.js
@@ -1,79 +1,78 @@
-    
-        const students = [
-            {
-                id: 2081A001,
-                name: 'Rosan Neupane',
-                class: '8',
-                results: {
-                    Nepali: 45,
-                    Math: 48,
-                    English: 44,
-                    Social: 47,
-                    Veda: 42,
-                    Karmakanda: 46,
-                    SanskritBhasha: 45,
-                    Vyakarana: 43
-                }
-            },
-            {
-                id: 2081A002,
-                name: 'Yaswanta Bhattarai',
-                class: '8',
-                results: {
-                    Nepali: 38,
-                    Math: 40,
-                    English: 35,
-                    Social: 39,
-                    Veda: 36,
-                    Karmakanda: 37,
-                    SanskritBhasha: 38,
-                    Vyakarana: 35
-                }
-                },
-                 {
-                id: 2081A003,
-                name: 'Diwakar Nepal',
-                class: '8',
-                results: {
-                    Nepali: 38,
-                    Math: 40,
-                    English: 35,
-                    Social: 39,
-                    Veda: 36,
-                    Karmakanda: 37,
-                    SanskritBhasha: 38,
-                    Vyakarana: 35
-                }
-                 },
-             {
-                id: 2081A004,
-                name: 'Vishnu Ghimire',
-                class: '8',
-                results: {
-                    Nepali: 38,
-                    Math: 40,
-                    English: 35,
-                    Social: 39,
-                    Veda: 36,
-                    Karmakanda: 37,
-                    SanskritBhasha: 38,
-                    Vyakarana: 35
-                }
-                },
-                  {
-                id: 2081A005,
-                name: 'Nishchal Kafle',
-                class: '8',
-                results: {
-                    Nepali: 38,
-                    Math: 40,
-                    English: 35,
-                    Social: 39,
-                    Veda: 36,
-                    Karmakanda: 37,
-                    SanskritBhasha: 38,
-                    Vyakarana: 35
-                }
-                    }
-            // Add more student data here
-        ];
+const students = [
+    {
+        id: '2081A001',
+        name: 'Rosan Neupane',
+        class: '8',
+        results: {
+            Nepali: 45,
+            Math: 48,
+            English: 44,
+            Social: 47,
+            Veda: 42,
+            Karmakanda: 46,
+            SanskritBhasha: 45,
+            Vyakarana: 43
+        }
+    },
+    {
+        id: '2081A002',
+        name: 'Yaswanta Bhattarai',
+        class: '8',
+        results: {
+            Nepali: 38,
+            Math: 40,
+            English: 35,
+            Social: 39,
+            Veda: 36,
+            Karmakanda: 37,
+            SanskritBhasha: 38,
+            Vyakarana: 35
+        }
+    },
+    {
+        id: '2081A003',
+        name: 'Diwakar Nepal',
+        class: '8',
+        results: {
+            Nepali: 38,
+            Math: 40,
+            English: 35,
+            Social: 39,
+            Veda: 36,
+            Karmakanda: 37,
+            SanskritBhasha: 38,
+            Vyakarana: 35
+        }
+    },
+    {
+        id: '2081A004',
+        name: 'Vishnu Ghimire',
+        class: '8',
+        results: {
+            Nepali: 38,
+            Math: 40,
+            English: 35,
+            Social: 39,
+            Veda: 36,
+            Karmakanda: 37,
+            SanskritBhasha: 38,
+            Vyakarana: 35
+        }
+    },
+    {
+        id: '2081A005',
+        name: 'Nishchal Kafle',
+        class: '8',
+        results: {
+            Nepali: 38,
+            Math: 40,
+            English: 35,
+            Social: 39,
+            Veda: 36,
+            Karmakanda: 37,
+            SanskritBhasha: 38,
+            Vyakarana: 35
+        }
+    }
+    // Add more student data here if needed
+];
